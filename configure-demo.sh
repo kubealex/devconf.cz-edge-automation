41b666e52341fa54ba17cd05b3fcd3bf2efe17e2
kubealex <al.rossi87@gmail.com>
2023-06-05 14:27:38 +0200

    update kubeconfig

--- configure-demo.sh
+++ configure-demo.sh
@@ -8,11 +8,11 @@ kind: Namespace
 metadata:
   labels:
     pod-security.kubernetes.io/audit: privileged
-    pod-security.kubernetes.io/audit-version: v1.24
+    pod-security.kubernetes.io/audit-version: latest
     pod-security.kubernetes.io/enforce: privileged
-    pod-security.kubernetes.io/enforce-version: v1.24
+    pod-security.kubernetes.io/enforce-version: latest
     pod-security.kubernetes.io/warn: privileged
-    pod-security.kubernetes.io/warn-version: v1.24
+    pod-security.kubernetes.io/warn-version: latest
   name: devconf-demo
 EOF
 oc adm policy add-scc-to-user -z eda-admin privileged -n devconf-demo
--- temperature-increase.sh
+++ temperature-increase.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-KUBECONFIG=~/microshift
+KUBECONFIG=$(pwd)/kubeconfig
+export KUBECONFIG=$(pwd)/kubeconfig
 
 oc apply -k kubernetes/sensors/patch
 oc delete pod -l kind=sensors -n devconf-demo
